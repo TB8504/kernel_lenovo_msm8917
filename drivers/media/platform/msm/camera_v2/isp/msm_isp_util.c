af1c098b00fa088bc2be0c309297c0e34ea33cba
TheScarastic <warabhishek@gmail.com>
2017-05-20 15:57:28 +0000

    Revert "msm: camera: isp: Handle standby mode for B+M"
    This reverts commit 12895349181eee37f58049a5f6c6872bf585e81c.
    
    Change-Id: Ib0f12c06d343c88c5e2f3b9e95b746cc52a1dca1

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -891,12 +891,14 @@ static long msm_isp_ioctl_unlocked(struct v4l2_subdev *sd,
 		mutex_unlock(&vfe_dev->buf_mgr->lock);
 		mutex_unlock(&vfe_dev->core_mutex);
 		break;
+#if !defined(CONFIG_MACH_LENOVO_TB8703)
 	case VIDIOC_MSM_ISP_CFG_HW_STATE:
 		mutex_lock(&vfe_dev->core_mutex);
 		rc = msm_isp_update_stream_bandwidth(vfe_dev,
 			*(enum msm_vfe_hw_state *)arg);
 		mutex_unlock(&vfe_dev->core_mutex);
 		break;
+#endif
 	case VIDIOC_MSM_ISP_AXI_HALT:
 		mutex_lock(&vfe_dev->core_mutex);
 		rc = msm_isp_axi_halt(vfe_dev, arg);
