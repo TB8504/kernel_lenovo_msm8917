86351443463f6ddd8810c6d66d3755f895e82745
Vitaliy Tomin <highwaystar.ru@gmail.com>
2019-07-22 16:34:12 +0800

    drivers: video: Import mdss changes for tb8704, tb8804, tb8504
    Change-Id: I2f468d441417752072c7c10cf6b6140fdc27d5f0

--- drivers/video/msm/mdss/mdss_mdp.c
+++ drivers/video/msm/mdss/mdss_mdp.c
@@ -4715,6 +4715,7 @@ static void mdss_mdp_config_cx_voltage(struct mdss_data_type *mdata, int enable)
 	}
 }
 
+#if defined(CONFIG_MACH_LENOVO_TB8703) || defined(CONFIG_MACH_LENOVO_TB8704) || defined(CONFIG_MACH_LENOVO_TB8804)
 int mdss_panel_get_boot_cfg(void)
 {
 	int rc;
@@ -4726,6 +4727,7 @@ int mdss_panel_get_boot_cfg(void)
 		rc = 0;
 	return rc;
 }
+#endif
 
 static int mdss_mdp_cx_ctrl(struct mdss_data_type *mdata, int enable)
 {
