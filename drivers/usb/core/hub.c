deea1db016a52f4b1bd7150de9f3a9a40f7797b8
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-09-01 09:15:37 +0200

    Merge branch 'LA.UM.8.6.c26' of https://source.codeaurora.org/quic/la/kernel/msm-3.18 into lineage-18.1

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -2255,6 +2255,7 @@ static int usb_enumerate_device_otg(struct usb_device *udev)
 			&& udev->parent == udev->bus->root_hub) {
 		struct usb_otg_descriptor	*desc = NULL;
 		struct usb_bus			*bus = udev->bus;
+		unsigned			port1 = udev->portnum;
 
 		/* descriptor may appear anywhere in config */
 		if (__usb_get_extra_descriptor (udev->rawdescriptors[0],
@@ -2289,8 +2290,20 @@ static int usb_enumerate_device_otg(struct usb_device *udev)
 					bus->b_hnp_enable = 0;
 				}
 			}
+		} else if (desc->bLength == sizeof
+				(struct usb_otg_descriptor)) {
+			/* Set a_alt_hnp_support for legacy otg device */
+			err = usb_control_msg(udev,
+				usb_sndctrlpipe(udev, 0),
+				USB_REQ_SET_FEATURE, 0,
+				USB_DEVICE_A_ALT_HNP_SUPPORT,
+				0, NULL, 0,
+				USB_CTRL_SET_TIMEOUT);
+			if (err < 0)
+				dev_err(&udev->dev,
+					"set a_alt_hnp_support failed: %d\n",
+					err);
 		}
-	}
 #endif
 	return err;
 }
